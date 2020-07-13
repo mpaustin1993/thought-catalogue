d0e71ae2ae2b651b06c60abb8efdc6f678ef8aad
Michael Austin <mpaustin1993@gmail.com>
2020-07-13 10:17:13 -0500

    edit button error

--- backend/app.js
+++ backend/app.js
@@ -29,44 +29,28 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-// only triggers incoming post requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  // actually going to save the post in our DB: meanauthapp
-  post.save().then((newPost) => {
-    // 201: everything okay, new resource created
-    res.status(201).json({
-      message: "Post added successfully",
-      postId: newPost._id,
-    });
-  });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
 });
 
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-  });
-  res.status(200).json({ message: "Post deleted!" });
-});
-
-// must use next() to get out of this function / have server send a reponse
-
-app.use("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents,
-    });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
+
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,65 +1,87 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Post } from './post.model';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
-@Injectable({ providedIn: 'root' })
+import { Post } from "./post.model";
+
+@Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  // define it & then pipe it w/ HttpClient
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  // were using the spread operator here b/c we want to copy the actual array, & edit that array
-  // get is generic so we can specify what kind of info we want to get back
   getPosts() {
     this.http
-      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .get<{ message: string; posts: any }>("http://localhost:3000/api/posts")
       .pipe(
-        map((postData) => {
-          return postData.posts.map((post) => {
+        map(postData => {
+          return postData.posts.map(post => {
             return {
               title: post.title,
               content: post.content,
-              id: post._id,
+              id: post._id
             };
           });
         })
       )
-      .subscribe((transformedPosts) => {
+      .subscribe(transformedPosts => {
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
 
-  getPostsUpdateListener() {
+  getPostUpdateListener() {
     return this.postsUpdated.asObservable();
+  }
+
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
   }
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
       .post<{ message: string; postId: string }>(
-        'http://localhost:3000/api/posts',
+        "http://localhost:3000/api/posts",
         post
       )
-      .subscribe((responseData) => {
+      .subscribe(responseData => {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
       });
   }
 
   deletePost(postId: string) {
     this.http
-      .delete('http://localhost:3000/api/posts/' + postId)
+      .delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
-        const updatedPosts = this.posts.filter((post) => post.id !== postId);
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
 }
+
+
