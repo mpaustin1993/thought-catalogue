d99ce6e43f0b2657349a128dca9ff136d32f9523
Michael Austin <mpaustin1993@gmail.com>
2020-07-16 20:09:44 -0500

    new URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    "http://meanstackapp-env.eba-uapi3xne.us-east-2.elasticbeanstalk.com/api",
+  apiUrl: "http://communityjournal.us-east-2.elasticbeanstalk.com/api",
 };
